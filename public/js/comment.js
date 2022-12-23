2bd3ccfd8d0fc1b052cbea3311e32f50bc9e4362
Bryant Trinh <trinhbryant@gmail.com>
2022-12-22 19:45:14 -0800

    comment js in public folder done

---
+++ public/js/comment.js
@@ -0,0 +1,30 @@
+// form handler for comment, 
+// DONE
+
+const commentForm = async function(event) {
+  event.preventDefault();
+}
+
+const postId = document.querySelector('input[name="post-id"]').value;
+
+const body = document.querySelector('textarea[name="comment-body"]').value;
+
+
+// if statement for body, we need to fetch api/comment annd our method is POST, stringify json object, the postId and body
+
+if (body) {
+  await fetch('/api/comment', {
+    method: 'POST',
+    body: JSON.stringify({
+      postId,
+      body
+    }),
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  });
+
+  window.location.reload();
+}
+
+document.querySelector('#new-comment-form').addEventListener('submit', commentForm);
