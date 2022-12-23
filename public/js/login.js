f13dd9e7ea5d1d2beb92243e1346ad8beccc6be0
Bryant Trinh <trinhbryant@gmail.com>
2022-12-22 20:06:13 -0800

    finished comment, edit, login.js

---
+++ public/js/login.js
@@ -0,0 +1,30 @@
+
+// DONE
+
+
+
+const loginForm = async function(event) {
+  event.preventDefault();
+
+
+const usernameEl = document.querySelector('#username-input-login');
+const passwordEl = document.querySelector('#password-input-login');
+
+const response = await fetch('/api/user/login', {
+  method: 'POST',
+  body: JSON.stringify({
+    username: usernameEl.value,
+
+    password: passwordEl.value,
+  }),
+  headers: { 'Content-Type': 'application/json' },
+});
+
+if (response.ok) {
+  document.window.replace('/dashboard');
+} else {
+  alert('Could not login');
+}
+};
+
+document.querySelector('#login-form').addEventListener('submit', loginForm);
